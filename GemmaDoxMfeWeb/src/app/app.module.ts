0e59f2938f805e6015f35310178db3e308f86f12
Curtis McMillin <curtis.w.mcmillin@gmail.com>
2017-09-28 15:05:39 -0500

    add schedule and search components

--- GemmaDoxMfeWeb/src/app/app.module.ts
+++ GemmaDoxMfeWeb/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule
