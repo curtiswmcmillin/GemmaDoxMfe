5ab936240fc5cc418d150cf2b75155669e7285ac
Curtis McMillin <curtis.w.mcmillin@gmail.com>
2017-09-28 14:29:49 -0500

    add UnitTest projects for Export,Search and Scheduler

---
+++ GemmaDoxMfe.Services.Search.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GemmaDoxMfe.Services.Search.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
